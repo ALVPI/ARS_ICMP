a2f127d46570d04950cc09095097ffdf7711c197
alvlope <81857522+ALVPI@users.noreply.github.com>
2025-12-17 12:00:33 +0100

    feat: modificando cosas para cumplir enunciado

--- echoICMP-g210.c
+++ echoICMP-g210.c
@@ -16,7 +16,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>   //for using ips 
-#include <netinet/ip_icmp.h> // icmp  
+#include <netinet/ip_icmp.h> // icmp
 #include <sys/time.h>      // for the  rtt of the client
 #include <errno.h>
 
@@ -56,6 +56,22 @@ uint16_t checksumCalculation(void *data, int len) {
 double diff_time (const struct timeval end_time, const struct timeval initial_time)
 {
     return ((end_time-> tv_sec - initial_time->tv_sec)*1000.0 )+ ((end_time->tv_usec- initial_time->tv_usec)/1000.0);
+}
+
+void handle_icmp_error(int type, int code) {
+    if (type == 3) {
+        printf("Destination Unreachable: ");
+        switch(code) {
+            case 0: printf("Net Unreachable (Type 3, Code 0)\n"); break;
+            case 1: printf("Host Unreachable (Type 3, Code 1)\n"); break; // [cite: 80]
+            case 3: printf("Port Unreachable (Type 3, Code 3)\n"); break;
+            default: printf("Error code %d\n", code); break;
+        }
+    } else if (type == 11) {
+        printf("Time Exceeded: Time-to-Live Exceeded in Transit (Type 11, Code 0)\n"); // [cite: 85]
+    } else {
+        printf("ICMP Error: Type %d, Code %d\n", type, code);
+    }
 }
 
 int main (int argc, char*argv[])
@@ -115,6 +131,11 @@ int main (int argc, char*argv[])
 
   memset(&local_addr,0,sizeof(local_addr));
   local_addr.sin_family = AF_INET;
+  if(inet_aton(argv[1], &host.sin_addr)==0)
+  {
+    perror("Invalid IP\n");
+    exit(1);
+  }
   local_addr.sin_addr.s_addr = INADDR_ANY;
 
   //Now we are going to bind the local interface
@@ -124,89 +145,75 @@ int main (int argc, char*argv[])
     exit(1);
   }
 
+  //Initial message
+  printf("PING %s: %zu bytes of data\n", argv[1], strlen(OPTIONAL_DATA));
    
 
   //Here is the main for loop for sending the petitions
-  for (int i=0; i<PING_COUNT;i++)
-  {
-    //We get the PID of the process 
-    pid_t pid = getpid();
-    
-    memset(&echo, 0, sizeof(Echo));
-    echo.icmpHdr.type = ICMP_ECHO_TYPE;
-    echo.icmpHdr.code = ICMP_ECHO_CODE;
-    echo.id = pid & 0xFFFF;
-    echo.sequence = i;
-    
-    memset(echo.payload, 0, LEN);
-    strncpy(echo.payload, OPTIONAL_DATA, LEN - 1);
+for (int seq = 1; seq <= PING_COUNT; ++seq) {
+    /* 1. construye datagrama ICMP plano */
+    uint8_t icmp_pkt[sizeof(struct icmphdr) + strlen(OPTIONAL_DATA)];
+    struct icmphdr *ic = (struct icmphdr *)icmp_pkt;
 
-    echo.icmpHdr.checksum = 0;
-    echo.icmpHdr.checksum = checksumCalculation(&echo, sizeof(Echo));
+    ic->type     = ICMP_ECHO_TYPE;                     // 8
+    ic->code     = ICMP_ECHO_CODE;                     // 0
+    ic->checksum = 0;                                  // se calcula después
+    ic->un.echo.id       = htons(getpid() & 0xFFFF);
+    ic->un.echo.sequence = htons(seq);
+    memcpy(icmp_pkt + sizeof(struct icmphdr), OPTIONAL_DATA, strlen(OPTIONAL_DATA));
 
+    ic->checksum = checksumCalculation(icmp_pkt, sizeof(icmp_pkt)); // bytes que viajan
 
-    /*We create the header
-
-    icmpPing->checksum =0; //we initialice the checksum to 0
-    icmpPing->type = ICMP_ECHO_TYPE;
-    icmpPing->code = ICMP_CODE;
-    icmpPing-> un.echo.id = htons(pid);
-    icmpPing-> un.echo.sequence = htons(seq);
-
-    //Add optional data
-    memcpy(snd_buffer + sizeof(struct icmphdr), OPTIONAL_DATA, PING_LEN);
-
-    icmp_packet->checksum = checksumCalculation((unsigned double *)icmp_packet, PING_LEN);
-    */
+    /* 2. envío y RTT */
+    struct timeval t_start, t_end;
     gettimeofday(&t_start, NULL);
-
-    if(sendto(sockfd, echo, sizeof(Echo), 0,
-        (struct sockaddr *)&host, sizeof(host))<0){
-
-          perror("Error al enviar el echo");
-          close(sockfd);
-          exit(EXIT_FAILURE);
-        }
-
-    FD_ZERO(&readfds);
-    FD_SET(sockfd, &readfds);
-
-    timeout.tv_sec = TIMEOUT_SEC;
-    timeout.tv_usec = 0;
-
-    int ready = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
-
-    if (ready == 0) {
-      printf("Echo %d: Timeout\n\n", i);
-      sleep(1);
-      continue;
+    if (sendto(sockfd, icmp_pkt, sizeof(icmp_pkt), 0,
+               (struct sockaddr *)&host, sizeof(host)) < 0) {
+        perror("sendto"); continue;
     }
-    else if (ready < 0) {
-      perror("Timeout expired...");
-      break;
-    }
+    sent++;
 
+    /* 3. espera respuesta con timeout */
+    fd_set rfds; struct timeval tv = { .tv_sec = TIMEOUT_SEC, .tv_usec = 0 };
+    FD_ZERO(&rfds); FD_SET(sockfd, &rfds);
+    int ready = select(sockfd + 1, &rfds, NULL, NULL, &tv);
+    if (ready == 0) { printf("Timeout Expired...\n"); continue; }
+    if (ready <  0) { perror("select"); break; }
 
-    socklen_t addr_len;
-    addr_len = sizeof(host);
-    int bytes = recvfrom(sockfd, rcv_buffer, SIZE, 0,
-        (struct sockaddr *)&host, &addr_len);
-  
-    if (bytes < 0) {
-      perror("recvfrom");
-      close(sockfd);
-      exit(EXIT_FAILURE);
-    }
-    
+    /* 4. recibe y extrae cabeceras */
+    socklen_t fromlen = sizeof(from_addr);
+    int n = recvfrom(sockfd, rcv_buffer, sizeof(rcv_buffer), 0,
+                     (struct sockaddr *)&from_addr, &fromlen);
+    if (n < 0) { perror("recvfrom"); continue; }
     gettimeofday(&t_end, NULL);
-    rtt = diff_time(&t_end, &t_start);
-    total_rtt +=rtt;
-    if(rtt<min_rtt) min_rtt = rtt;
-    if(max_rtt<rtt) max_rtt = rtt;
-    recv_count ++;
-    printf("Echo Reply from %s: seq=%d TTL=%d RTT%.3f ms\n", inet_ntoa(host.sin_addr), seq, ip->ip_ttl, rtt);
-  }
 
+    struct iphdr   *ip     = (struct iphdr *)rcv_buffer;
+    int ip_hdr_len         = ip->ihl * 4;
+    struct icmphdr *icmp_r = (struct icmphdr *)(rcv_buffer + ip_hdr_len);
+
+    /* 5. procesa respuesta */
+    if (icmp_r->type == 0 && icmp_r->code == 0) {               // Echo Reply
+        if (icmp_r->un.echo.id   == ic->un.echo.id &&
+            icmp_r->un.echo.sequence == ic->un.echo.sequence) {
+            double rtt_ms = diff_time(&t_end, &t_start);
+            total_rtt += rtt_ms;
+            if (rtt_ms < min_rtt) min_rtt = rtt_ms;
+            if (rtt_ms > max_rtt) max_rtt = rtt_ms;
+            recv_count++;
+            printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n",
+                   n, inet_ntoa(from_addr.sin_addr), seq, ip->ttl, rtt_ms);
+        }
+    }
+    else if (icmp_r->type == ICMP_ECHO_TYPE) {                  // loopback sin procesar
+        printf("ICMP Datagram Not Processed...\n");
+    }
+    else {
+        handle_icmp_error(icmp_r->type, icmp_r->code);          // errores varios
+    }
+
+    /* 6. espera 1 s entre envíos (salvo el último) */
+    if (seq < PING_COUNT - 1) sleep(SLEEP_TIME);
+}
   //We print the final stats
   printf("--- %s estadisticas ---\n", argv[1]);
   printf("%d packets transmitted %d recieved %d packet loss\n", 
@@ -218,3 +225,4 @@ int main (int argc, char*argv[])
     printf("rtt min/avg/max = %.3f/%.3f/%.3f ms\n",min_rtt, total_rtt/recv_count, max_rtt);
   }
   return 0;
+}
